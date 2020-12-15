dc54e61f1e3d062cef615ffab56521d3ffd7a91f
adam <athornton@gmail.com>
2020-12-15 11:38:30 -0700

    Add add-pull-secrets tool.

---
+++ tools/addpullsecret/add-pull-secrets.bash
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+# Requires Bash 4 because we use hashes; brew install bash if you're on
+#  OS X (it's a GPL3 thing)
+
+function usage() {
+    echo 1>&2 "Usage: $0 lsp-deploy-directory"
+    exit 1
+}
+
+bv=$(echo ${BASH_VERSION} | cut -d '.' -f 1)
+if [ ${bv} -lt 4 ]; then
+    echo 1>&2 "$0 requires at least bash version 4"
+    exit 2
+fi
+
+# Customize as necessary
+topdir=$1
+
+if [ -z "${topdir}" ]; then
+    usage
+fi
+if [ -n "$2" ]; then
+    usage
+fi
+
+# This builds a list of services that have associated namespaces.
+#  For instance, cert-manager and cert-issuer share a namespace; we only
+#  inject a vault secret for pull once per namespace
+svcs="argocd cert-manager exposurelog gafaelfawr influxdb kapacitor"
+svcs="${svcs} landing-page mobu nginx-ingress nublado obstap portal"
+svcs="${svcs} postgres tap vault-secrets-operator wf"
+
+# This is a list of environments.
+envs="base bleed gold-leader idfdev int kueyen minikube nts nublado"
+envs="${envs} red-five rogue-two stable summit tucson-teststand"
+
+# This is what I have run it with so far.
+#envs="nublado"
+
+IFS='' read -r -d '' addreq <<'EOF'
+- name: pull-secret
+  version: ">=0.1.2"
+  repository: https://lsst-sqre.github.io/charts/
+EOF
+
+declare -A pull_secret
+for e in ${envs}; do
+    np="${topdir}/services/nublado/values-${e}.yaml"
+    if ! [ -e ${np} ]; then
+	echo 1>&2 "No nublado to query for secret path in env ${e}!"
+	continue
+    fi
+    tops=$(grep "secret/k8s_operator" ${np} | head -1 | cut -d / -f 3 )
+    if [ -z "${tops}" ] ;then
+	echo 1>&2 "Could not determine vault secret path for ${e}."
+	continue
+    fi
+    pull_secret[${e}]="secret/k8s_operator/${tops}/pull-secret"
+done
+for s in ${svcs}; do
+    svcdir="${topdir}/services/${s}"
+    for e in ${envs}; do
+	psp="${pull_secret[${e}]}"
+	if [ -z "${psp}" ]; then
+	    echo 1>&2 "No vault secret path for ${e}."
+	    continue
+	fi
+	IFS='' read -r -d '' addsec <<EOF
+
+pull-secret:
+  enabled: true
+  path: ${psp}
+EOF
+	efile="${svcdir}/values-${e}.yaml"
+	# if it doesn't exist, then it's OK to create it.
+	if ! [ -e ${efile} ]; then
+	    if [ -e ${svcdir}/values.yaml ]; then
+		cp ${svcdir}/values.yaml ${efile}
+	    fi
+	fi
+	grep -q "^pull-secret:" ${efile} 2>/dev/null
+	rc=$?
+	if [ ${rc} -eq 0 ] ; then
+	    echo 1>&2 "${efile} already has pull-secret."
+	else
+	    echo -n "${addsec}" >> ${efile}
+	fi
+    done
+    # Add pull-secret to requirements file.
+    # nginx-ingress has its dependencies right in Chart.yaml
+    rfile="${svcdir}/requirements.yaml"
+    if [ "${s}" == "nginx-ingress" ]; then
+	rfile="${svcdir}/Chart.yaml"
+    fi
+    grep -q "pull-secret" ${rfile} 2>/dev/null
+    rc=$?
+    if [ ${rc} -eq 0 ] ; then
+	echo 1>&2 "${rfile} already has pull-secret."
+    else
+	echo -n "${addreq}" >> ${rfile}
+    fi
+done
+
+exit 0
