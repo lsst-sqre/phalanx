cbbe831067b3cc54c0e7aa805bfdaa398ef5d7ce
Christine Banek <cbanek@gmail.com>
2021-02-23 13:51:51 -0700

    [DM-28839] Add script to get secrets out of vault
    This will be helpful to know what the secrets are by
    just looking at a file.  Then we can edit them as we
    need, and then push them back into vault.

---
+++ installer/get_secrets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+ENVIRONMENT=${1:?"Usage: get_secrets.sh ENVIRONMENT"}
+
+mkdir -p secrets
+
+COMPONENTS=`vault kv list --format=yaml secret/k8s_operator/$ENVIRONMENT | yq -r '.[]'`
+for SECRET in $COMPONENTS
+do
+  vault kv get --field=data --format=json secret/k8s_operator/$ENVIRONMENT/$SECRET > secrets/$SECRET
+done
