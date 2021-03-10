d267aa870044d672174e5731dcbccb7173642eef
Christine Banek <cbanek@gmail.com>
2021-03-09 17:54:49 -0700

    [DM-28839] Refactor into a class
    Just to get a bunch of the shared state out of there and cleaned up.

--- installer/generate_secrets.py
+++ installer/generate_secrets.py
@@ -15,214 +15,212 @@ import secrets
 import yaml
 
 
-def input_field(s, component, name, description):
-    default = s[component].get(name, "")
-    prompt_string = f"[{component} {name}] ({description}): [current: {default}] "
-    input_string = input(prompt_string)
+class SecretGenerator:
+    def __init__(self, regenerate):
+        self.secrets = {}
+        self.regenerate = regenerate
 
-    if input_string:
-        s[component][name] = input_string
+    def generate(self):
+        self._pull_secret()
+        self._postgres()
+        self._log()
+        self._tap()
+        self._nublado()
+        self._nublado2()
+        self._mobu()
+        self._gafaelfawr()
+        self._argocd()
+        self._portal()
+
+        self.input_field("cert-manager", "enabled", "Use cert-manager? (y/n):")
+        use_cert_manager = self.secrets["cert-manager"]["enabled"]
+        if use_cert_manager == "y":
+            self._cert_manager()
+        elif use_cert_manager == "n":
+            self._ingress_nginx()
+        else:
+            raise Exception("Invalid cert manager enabled")
+
+    def load(self):
+        for f in Path('secrets').iterdir():
+            print(f"Loading {f}")
+            component = os.path.basename(f)
+            self.secrets[component] = json.loads(f.read_text())
+
+    def save(self):
+        os.makedirs("secrets", exist_ok=True)
+
+        for k, v in self.secrets.items():
+            with open(f"secrets/{k}", "w") as f:
+                f.write(json.dumps(v))
+
+    def input_field(self, component, name, description):
+        default = self.secrets[component].get(name, "")
+        prompt_string = f"[{component} {name}] ({description}): [current: {default}] "
+        input_string = input(prompt_string)
+
+        if input_string:
+            self.secrets[component][name] = input_string
 
 
-def input_file(s, component, name, description):
-    current = s.get(component, {}).get(name, "")
-    print(f"[{component} {name}] ({description})")
-    print(f"Current contents:\n{current}")
-    prompt_string = f"New filename with contents (empty to not change): "
-    fname = input(prompt_string)
+    def input_file(self, component, name, description):
+        current = self.secrets.get(component, {}).get(name, "")
+        print(f"[{component} {name}] ({description})")
+        print(f"Current contents:\n{current}")
+        prompt_string = f"New filename with contents (empty to not change): "
+        fname = input(prompt_string)
 
-    if fname:
-        with open(fname, "r") as f:
-            s[component][name] = f.read()
+        if fname:
+            with open(fname, "r") as f:
+                self.secrets[component][name] = f.read()
 
 
-def set_generated_secret(s, component, name, new_value, regenerate):
-    if regenerate:
-        s[component][name] = new_value
+    def _set_generated(self, component, name, new_value):
+        if self.regenerate:
+            self.secrets[component][name] = new_value
 
-def generate_tap_secrets(s, regen):
-    input_field(s, "tap", "slack_webhook_url", "slack webhook url for querymonkey")
-    input_file(s, "tap", "google_creds.json", "file containing google service account credentials")
+    def _tap(self):
+        self.input_field("tap", "slack_webhook_url", "slack webhook url for querymonkey")
+        self.input_file("tap", "google_creds.json", "file containing google service account credentials")
 
-def generate_log_secrets(s, regen):
-    def gen_pw_and_hash():
-        pw = secrets.token_hex(16)
+    def _log(self):
+        def gen_pw_and_hash():
+            pw = secrets.token_hex(16)
+            h = bcrypt.hashpw(
+                pw.encode("ascii"), bcrypt.gensalt(rounds=15)
+            ).decode("ascii")
+            return (pw, h)
+
+        admin = gen_pw_and_hash()
+        kibana = gen_pw_and_hash()
+        logstash = gen_pw_and_hash()
+
+        internal_users = {
+            "_meta": {
+                "type": "internalusers",
+                "config_version": 2,
+            },
+            "admin": {
+                "hash": admin[1],
+                "reserved": True,
+                "backend_roles": ["admin"],
+                "description": "Admin user",
+            },
+            "kibana": {
+                "hash": kibana[1],
+                "backend_roles": ["kibanauser"],
+                "description": "Kibana user",
+            },
+            "logstash": {
+                "hash": logstash[1],
+                "reserved": False,
+                "backend_roles": ["logstash"],
+                "description": "Logstash writing user",
+            },
+        }
+
+        # username / password / cookie is the elasticsearch admin user,
+        # and this key is unfortunately hardcoded by the chart.
+        # fluentd and kibana passwords are also passed through, but these
+        # are tied to the hashes that are present in the internal_users
+        # yaml file, where the bcrypt'd password is stored.
+        self._set_generated("log", "username", "admin")
+        self._set_generated("log", "password", admin[0])
+        self._set_generated("log", "cookie", secrets.token_urlsafe(32))
+        self._set_generated("log", "internal_users.yml", yaml.dump(internal_users))
+        self._set_generated("log", "logstash-password", logstash[0])
+        self._set_generated("log", "kibana-password", kibana[0])
+
+
+    def _postgres(self):
+        self._set_generated("postgres", "exposurelog_password", secrets.token_hex(32))
+        self._set_generated("postgres", "jupyterhub_password", secrets.token_hex(32))
+        self._set_generated("postgres", "root_password", secrets.token_hex(64))
+
+
+    def _nublado2(self):
+        crypto_key = ";".join([secrets.token_hex(32), secrets.token_hex(32)])
+        self._set_generated("nublado2", "crypto_key", crypto_key)
+        self._set_generated("nublado2", "proxy_token", secrets.token_hex(32))
+
+
+    def _nublado(self):
+        crypto_key = ";".join([secrets.token_hex(32), secrets.token_hex(32)])
+        self._set_generated("nublado", "jupyterhub_crypto_key", crypto_key)
+        self._set_generated("nublado", "configproxy_auth_token", secrets.token_hex(32))
+
+        # Pluck the password out of the postgres portion to generate our connect string.
+        db_pass = self.secrets["postgres"]["jupyterhub_password"]
+        session_db_url = f"postgres://jovyan:{db_pass}@postgres.postgres/jupyterhub"
+        self.secrets["nublado"]["session_db_url"] = session_db_url
+
+
+    def _mobu(self):
+        self.input_field("mobu", "ALERT_HOOK", "Slack webhook for reporting mobu alerts.  Or use None for no alerting.")
+
+
+    def _cert_manager(self):
+        self.input_field("cert-manager", "aws-secret-access-key", "AWS secret access key for zone for DNS cert solver.")
+
+
+    def _gafaelfawr(self):
+        key = rsa.generate_private_key(
+            backend=default_backend(), public_exponent=65537, key_size=2048
+        )
+
+        key_bytes = key.private_bytes(
+            serialization.Encoding.PEM,
+            serialization.PrivateFormat.PKCS8,
+            serialization.NoEncryption(),
+        )
+
+        self._set_generated("gafaelfawr", "redis-password", os.urandom(32).hex())
+        self._set_generated("gafaelfawr", "session-secret", Fernet.generate_key().decode())
+        self._set_generated("gafaelfawr", "signing-key", key_bytes.decode())
+
+        self.input_field("gafaelfawr", "auth_type", "Use cilogon or github?")
+        auth_type = self.secrets["gafaelfawr"]["auth_type"]
+        if auth_type == "cilogon":
+            self.input_field("gafaelfawr", "cilogon-client-secret", "CILogon client secret")
+        elif auth_type == "github":
+            self.input_field("gafaelfawr", "github-client-secret", "GitHub client secret")
+        else:
+            raise Exception("Invalid auth provider")
+
+
+    def _pull_secret(self):
+        self.input_file("pull-secret", ".dockerconfigjson", ".docker/config.json to pull images")
+
+
+    def _ingress_nginx(self):
+        self.input_file("ingress-nginx", "tls.key", "Certificate private key")
+        self.input_file("ingress-nginx", "tls.crt", "Certificate chain")
+
+
+    def _argocd(self):
+        self.input_field("installer", "argocd.admin.plaintext_password", "Admin password for ArgoCD?")
+        pw = self.secrets["installer"]["argocd.admin.plaintext_password"]
         h = bcrypt.hashpw(
             pw.encode("ascii"), bcrypt.gensalt(rounds=15)
         ).decode("ascii")
-        return (pw, h)
+        now_time = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    admin = gen_pw_and_hash()
-    kibana = gen_pw_and_hash()
-    logstash = gen_pw_and_hash()
-
-    internal_users = {
-        "_meta": {
-            "type": "internalusers",
-            "config_version": 2,
-        },
-        "admin": {
-            "hash": admin[1],
-            "reserved": True,
-            "backend_roles": ["admin"],
-            "description": "Admin user",
-        },
-        "kibana": {
-            "hash": kibana[1],
-            "backend_roles": ["kibanauser"],
-            "description": "Kibana user",
-        },
-        "logstash": {
-            "hash": logstash[1],
-            "reserved": False,
-            "backend_roles": ["logstash"],
-            "description": "Logstash writing user",
-        },
-    }
-
-    # username / password / cookie is the elasticsearch admin user,
-    # and this key is unfortunately hardcoded by the chart.
-    # fluentd and kibana passwords are also passed through, but these
-    # are tied to the hashes that are present in the internal_users
-    # yaml file, where the bcrypt'd password is stored.
-    set_generated_secret(s, "log", "username", "admin", regen)
-    set_generated_secret(s, "log", "password", admin[0], regen)
-    set_generated_secret(s, "log", "cookie", secrets.token_urlsafe(32), regen)
-    set_generated_secret(s, "log", "internal_users.yml", yaml.dump(internal_users), regen)
-    set_generated_secret(s, "log", "logstash-password", logstash[0], regen)
-    set_generated_secret(s, "log", "kibana-password", kibana[0], regen)
+        self._set_generated("argocd", "admin.password", h)
+        self._set_generated("argocd", "admin.passwordMtime", now_time)
+        self._set_generated("argocd", "server.secretkey", secrets.token_hex(16))
 
 
-def generate_postgres_secrets(s, regen):
-    set_generated_secret(s, "postgres", "exposurelog_password", secrets.token_hex(32), regen)
-    set_generated_secret(s, "postgres", "jupyterhub_password", secrets.token_hex(32), regen)
-    set_generated_secret(s, "postgres", "root_password", secrets.token_hex(64), regen)
-
-
-def generate_nublado2_secrets(s, regen):
-    crypto_key = ";".join([secrets.token_hex(32), secrets.token_hex(32)])
-    set_generated_secret(s, "nublado2", "crypto_key", crypto_key, regen)
-    set_generated_secret(s, "nublado2", "proxy_token", secrets.token_hex(32), regen)
-
-
-def generate_nublado_secrets(s, regen):
-    crypto_key = ";".join([secrets.token_hex(32), secrets.token_hex(32)])
-    set_generated_secret(s, "nublado", "jupyterhub_crypto_key", crypto_key, regen)
-    set_generated_secret(s, "nublado", "configproxy_auth_token", secrets.token_hex(32), regen)
-
-    # Pluck the password out of the postgres portion to generate our connect string.
-    db_pass = s["postgres"]["jupyterhub_password"]
-    session_db_url = f"postgres://jovyan:{db_pass}@postgres.postgres/jupyterhub"
-    s["nublado"]["session_db_url"] = session_db_url
-
-
-def generate_mobu_secrets(s, regen):
-    input_field(s, "mobu", "ALERT_HOOK", "Slack webhook for reporting mobu alerts.  Or use None for no alerting.")
-
-
-def generate_cert_manager_secrets(s, regen):
-    input_field(s, "cert-manager", "aws-secret-access-key", "AWS secret access key for zone for DNS cert solver.")
-
-
-def generate_gafaelfawr_secrets(s, regen):
-    key = rsa.generate_private_key(
-        backend=default_backend(), public_exponent=65537, key_size=2048
-    )
-
-    key_bytes = key.private_bytes(
-        serialization.Encoding.PEM,
-        serialization.PrivateFormat.PKCS8,
-        serialization.NoEncryption(),
-    )
-
-    set_generated_secret(s, "gafaelfawr", "redis-password", os.urandom(32).hex(), regen)
-    set_generated_secret(s, "gafaelfawr", "session-secret", Fernet.generate_key().decode(), regen)
-    set_generated_secret(s, "gafaelfawr", "signing-key", key_bytes.decode(), regen)
-
-    input_field(s, "gafaelfawr", "auth_type", "Use cilogon or github?")
-    auth_type = s["gafaelfawr"]["auth_type"]
-    if auth_type == "cilogon":
-        input_field(s, "gafaelfawr", "cilogon-client-secret", "CILogon client secret")
-    elif auth_type == "github":
-        input_field(s, "gafaelfawr", "github-client-secret", "GitHub client secret")
-    else:
-        raise Exception("Invalid auth provider")
-
-
-def generate_pull_secret(s, regen):
-    input_file(s, "pull-secret", ".dockerconfigjson", ".docker/config.json to pull images")
-
-
-def generate_ingress_nginx_secrets(s, regen):
-    input_file(s, "ingress-nginx", "tls.key", "Certificate private key")
-    input_file(s, "ingress-nginx", "tls.crt", "Certificate chain")
-
-
-def generate_argocd_secrets(s, regen):
-    input_field(s, "installer", "argocd.admin.plaintext_password", "Admin password for ArgoCD?")
-    pw = s["installer"]["argocd.admin.plaintext_password"]
-    h = bcrypt.hashpw(
-        pw.encode("ascii"), bcrypt.gensalt(rounds=15)
-    ).decode("ascii")
-    now_time = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-    set_generated_secret(s, "argocd", "admin.password", h, regen)
-    set_generated_secret(s, "argocd", "admin.passwordMtime", now_time, regen)
-    set_generated_secret(s, "argocd", "server.secretkey", secrets.token_hex(16), regen)
-
-
-def generate_portal_secrets(s, regen):
-    pw = secrets.token_hex(32)
-
-    set_generated_secret(s, "portal", "ADMIN_PASSWORD", pw, regen)
-
-
-def load_current_secrets():
-    s = defaultdict(dict)
-
-    for f in Path('secrets').iterdir():
-        print(f"Loading {f}")
-        component = os.path.basename(f)
-        s[component] = json.loads(f.read_text())
-
-    return s
-
-def generate_secrets(regen):
-    s = load_current_secrets()
-    generate_pull_secret(s, regen)
-    generate_postgres_secrets(s, regen)
-    generate_log_secrets(s, regen)
-    generate_tap_secrets(s, regen)
-    generate_nublado_secrets(s, regen)
-    generate_nublado2_secrets(s, regen)
-    generate_mobu_secrets(s, regen)
-    generate_gafaelfawr_secrets(s, regen)
-    generate_argocd_secrets(s, regen)
-    generate_portal_secrets(s, regen)
-
-    input_field(s, "cert-manager", "enabled", "Use cert-manager? (y/n):")
-    use_cert_manager = s["cert-manager"]["enabled"]
-    if use_cert_manager == "y":
-        generate_cert_manager_secrets(s, regen)
-    elif use_cert_manager == "n":
-        generate_ingress_nginx_secrets(s, regen)
-    else:
-        raise Exception("Invalid cert manager enabled")
-
-    return s
-
-
-def generate_files(s):
-    print("Generating secrets files in secrets/")
-    os.makedirs("secrets", exist_ok=True)
-
-    for k, v in s.items():
-        with open(f"secrets/{k}", "w") as f:
-            f.write(json.dumps(v))
+    def _portal(self):
+        pw = secrets.token_hex(32)
+        self._set_generated("portal", "ADMIN_PASSWORD", pw)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="generate_secrets")
     parser.add_argument('--regenerate', default=False, action='store_true')
     args = parser.parse_args()
-    generate_files(generate_secrets(args.regenerate))
+
+    sg = SecretGenerator(args.regenerate)
+    sg.load()
+    sg.generate()
+    sg.save()
