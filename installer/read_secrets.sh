37e47ed285dee14d27e89ce11c8639ac4fac91cb
Christine Banek <cbanek@gmail.com>
2021-03-11 17:29:11 -0700

    [DM-28839] Ignore ts/ and efd/ parts of vault
    Otherwise we'll pull it down and maybe get an error about it being
    a directory.

--- installer/read_secrets.sh
+++ installer/read_secrets.sh
@@ -7,5 +7,7 @@ mkdir -p secrets
 COMPONENTS=`vault kv list --format=yaml secret/k8s_operator/$ENVIRONMENT | yq -r '.[]'`
 for SECRET in $COMPONENTS
 do
-  vault kv get --field=data --format=json secret/k8s_operator/$ENVIRONMENT/$SECRET > secrets/$SECRET
+  if [ $SECRET != "efd/" ] && [ $SECRET != "ts/" ]; then
+    vault kv get --field=data --format=json secret/k8s_operator/$ENVIRONMENT/$SECRET > secrets/$SECRET
+  fi
 done
