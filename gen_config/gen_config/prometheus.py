fe52c2ea07dfc1b73a398565665498f9f66a8357
adam <athornton@lsst.org>
2022-04-11 15:16:12 -0700

    change metrics endpoints back

--- gen_config/gen_config/prometheus.py
+++ gen_config/gen_config/prometheus.py
@@ -1,10 +1,10 @@
 prometheus_config = {
     "argocd": {
-        "application_controller": "http://argocd-application-controller.argocd.svc:8082/metrics",
-        "notifications_controller": "http://argocd-notifications-controller.argocd.svc:9001/metrics",
-        "redis": "http://argocd-redis.argocd.svc:9121/metrics",
-        "repo_server": "http://argocd-repo-server.argocd.svc:8084/metrics",
-        "server": "http://argocd-server.argocd.svc:8083/metrics",
+        "application_controller": "http://argocd-application-controller-metrics.argocd.svc:8082/metrics",
+        "notifications_controller": "http://argocd-notifications-controller-metrics.argocd.svc:9001/metrics",
+        "redis": "http://argocd-redis-metrics.argocd.svc:9121/metrics",
+        "repo_server": "http://argocd-repo-server-metrics.argocd.svc:8084/metrics",
+        "server": "http://argocd-server-metrics.argocd.svc:8083/metrics",
         },
     "nublado2": {
         "hub": "http://hub.nublado2:8081/metrics",
