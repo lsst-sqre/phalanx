122568e409e8c6c00e403e65f8d116fbdcfa4903
Jonathan Sick <jsick@lsst.org>
2022-10-18 15:23:07 -0400

    Add crossref roles and directives for app topics
    This makes it easier to cross reference the standard app topic types.

--- src/phalanx/docs/crossref.py
+++ src/phalanx/docs/crossref.py
@@ -9,6 +9,7 @@ __all__ = ["setup"]
 
 def setup(app: Sphinx) -> None:
     """Set up the Phalan cross-referencing extensions."""
+    # Cross reference an environment's homepage
     app.add_crossref_type(
         "px-env",
         "px-env",
@@ -17,9 +18,55 @@ def setup(app: Sphinx) -> None:
         objname="",
         override=False,
     )
+    # Cross reference an app's homepage
     app.add_crossref_type(
         "px-app",
         "px-app",
+        indextemplate="single: %s",
+        ref_nodeclass=None,
+        objname="",
+        override=False,
+    )
+    # Cross reference an app's architectural notes page
+    app.add_crossref_type(
+        "px-app-notes",
+        "px-app-notes",
+        indextemplate="single: %s",
+        ref_nodeclass=None,
+        objname="",
+        override=False,
+    )
+    # Cross reference an app's bootstrapping page
+    app.add_crossref_type(
+        "px-app-bootstrap",
+        "px-app-bootstrap",
+        indextemplate="single: %s",
+        ref_nodeclass=None,
+        objname="",
+        override=False,
+    )
+    # Cross reference an app's upgrade page
+    app.add_crossref_type(
+        "px-app-upgrade",
+        "px-app-upgrade",
+        indextemplate="single: %s",
+        ref_nodeclass=None,
+        objname="",
+        override=False,
+    )
+    # Cross reference an app's troubleshooting page
+    app.add_crossref_type(
+        "px-app-troubleshooting",
+        "px-app-troubleshooting",
+        indextemplate="single: %s",
+        ref_nodeclass=None,
+        objname="",
+        override=False,
+    )
+    # Cross reference an app's Helm values page
+    app.add_crossref_type(
+        "px-app-values",
+        "px-app-values",
         indextemplate="single: %s",
         ref_nodeclass=None,
         objname="",
