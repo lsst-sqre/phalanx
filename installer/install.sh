e385134bd3ec66ee26cb40f62bdd49637f2e9b29
pav511 <38131208+pav511@users.noreply.github.com>
2023-01-27 11:46:42 -0800

    revert install script to token install rather than vault approle

--- installer/install.sh
+++ installer/install.sh
@@ -1,22 +1,17 @@
 #!/bin/bash -xe
-USAGE="Usage: ./install.sh ENVIRONMENT VAULT_ROLE_ID_PATH VAULT_SECRET_ID"
+USAGE="Usage: ./install.sh ENVIRONMENT VAULT_TOKEN"
 ENVIRONMENT=${1:?$USAGE}
 #export VAULT_TOKEN=${2:?$USAGE}
-export VAULT_ROLE_ID_PATH=${2:?$USAGE}
-echo "VAULT_ROLE_ID_PATH=${VAULT_ROLE_ID_PATH}"
-export VAULT_SECRET_ID=${3:?$USAGE}
-echo "VAULT_SECRET_ID=${VAULT_SECRET_ID}"
 export VAULT_ADDR=${VAULT_ADDR:-https://vault.lsst.codes}
 #VAULT_PATH_PREFIX=`yq -r .vault_path_prefix ../science-platform/values-$ENVIRONMENT.yaml`
-VAULT_PATH_PREFIX=$(cat ../science-platform/values-$ENVIRONMENT.yaml | grep vault_path_prefix | awk '{print $2}')
+VAULT_PATH_PREFIX=$(cat ../science-platform/values-usdfdev.yaml | grep vault_path_prefix | awk '{print $2}')
 ARGOCD_PASSWORD=`vault kv get --field=argocd.admin.plaintext_password $VAULT_PATH_PREFIX/installer`
 
 GIT_URL=`git config --get remote.origin.url`
-HTTP_URL=$( echo "$GIT_URL" | sed s%git@%https://% | sed s%github.com:%github.com/% ).git
 # Github runs in a detached head state, but sets GITHUB_REF,
 # extract the branch from it.  If we're there, use that branch.
 # git branch --show-current will return empty in deatached head.
-GIT_BRANCH=${GITHUB_HEAD_REF:-`git rev-parse --abbrev-ref HEAD`}
+GIT_BRANCH=${GITHUB_HEAD_REF:-`git branch --show-current`}
 
 echo "Set VAULT_TOKEN in a secret for vault-secrets-operator..."
 # The namespace may not exist already, but don't error if it does.
@@ -28,38 +23,27 @@ kubectl create ns vault-secrets-operator || true
 #  --dry-run -o yaml | kubectl apply -f -
 kubectl create secret generic vault-secrets-operator \
   --namespace vault-secrets-operator \
-  --from-literal=VAULT_ROLE_ID=$(vault read --format=json ${VAULT_ROLE_ID_PATH}/role-id | jq -M .data.role_id  | sed 's/"//g') \
-  --from-literal=VAULT_SECRET_ID=${VAULT_SECRET_ID} \
+  --from-literal=VAULT_ROLE_ID=$(vault read --format=json auth/approle/role/rubin-data-dev.slac.stanford.edu/role-id | jq -M .data.role_id  | sed 's/"//g') \
+  --from-literal=VAULT_SECRET_ID=$(vault write -f --format=json auth/approle/role/rubin-data-dev.slac.stanford.edu/secret-id | jq .data.secret_id | sed 's/"//g') \
   --from-literal=VAULT_TOKEN_MAX_TTL=600 \
   --dry-run=client -o yaml | kubectl apply -f -
   
-echo "Set up docker pull secret for vault-secrets-operator..."
-vault kv get --field=.dockerconfigjson $VAULT_PATH_PREFIX/pull-secret > docker-creds
-kubectl create secret generic pull-secret -n vault-secrets-operator \
-    --from-file=.dockerconfigjson=docker-creds \
-    --type=kubernetes.io/dockerconfigjson \
-    --dry-run=client -o yaml | kubectl apply -f -
-
-
 echo "Update / install vault-secrets-operator..."
 # ArgoCD depends on pull-secret, which depends on vault-secrets-operator.
 helm dependency update ../services/vault-secrets-operator
 helm upgrade vault-secrets-operator ../services/vault-secrets-operator \
   --install \
-  --values ../services/vault-secrets-operator/values.yaml \
   --values ../services/vault-secrets-operator/values-$ENVIRONMENT.yaml \
   --create-namespace \
   --namespace vault-secrets-operator \
   --timeout 15m \
   --wait
 
-echo "Update / install argocd using helm..."
+echo "Update / install argocd using helm3..."
 helm dependency update ../services/argocd
 helm upgrade argocd ../services/argocd \
   --install \
-  --values ../services/argocd/values.yaml \
   --values ../services/argocd/values-$ENVIRONMENT.yaml \
-  --set global.vaultSecretsPath="$VAULT_PATH_PREFIX" \
   --create-namespace \
   --namespace argocd \
   --timeout 15m \
@@ -75,14 +59,14 @@ argocd login \
 
 echo "Creating top level application"
 argocd app create science-platform \
-  --repo $HTTP_URL \
+  --repo $GIT_URL \
   --path science-platform --dest-namespace default \
   --dest-server https://kubernetes.default.svc \
   --upsert \
   --revision $GIT_BRANCH \
   --port-forward \
   --port-forward-namespace argocd \
-  --helm-set repoURL=$HTTP_URL \
+  --helm-set repoURL=$GIT_URL \
   --helm-set revision=$GIT_BRANCH \
   --values values-$ENVIRONMENT.yaml
 
@@ -91,48 +75,27 @@ argocd app sync science-platform \
   --port-forward-namespace argocd
 
 echo "Syncing critical early applications"
-if [ $(yq -r .ingress_nginx.enabled ../science-platform/values-$ENVIRONMENT.yaml) == "true" ];
-then
-  echo "Syncing ingress-nginx..."
-  argocd app sync ingress-nginx \
-    --plaintext \
-    --port-forward \
-    --port-forward-namespace argocd
-fi
+argocd app sync ingress-nginx \
+  --port-forward \
+  --port-forward-namespace argocd
 
 # Wait for the cert-manager's webhook to finish deploying by running
-# kubectl, argocd's sync doesn't seem to wait for this to finish.
-if [ $(yq -r .cert_manager.enabled ../science-platform/values-$ENVIRONMENT.yaml) == "true" ];
-then
-  echo "Syncing cert-manager..."
-  argocd app sync cert-manager \
-    --plaintext \
-    --port-forward \
-    --port-forward-namespace argocd && \
-    kubectl -n cert-manager rollout status deploy/cert-manager-webhook
-fi
+# kubectl.  argocd's sync doesn't seem to wait for this to finish.
+# This is a little tricky on the bash.  If cert-manager isn't used,
+# the argocd command will fail, then we will continue.  If it doesn't
+# fail, wait for it to finish.
+(argocd app sync cert-manager \
+  --port-forward \
+  --port-forward-namespace argocd && \
+kubectl -n cert-manager rollout status deploy/cert-manager-webhook) || true
 
-if [ $(yq -r .postgres.enabled ../science-platform/values-$ENVIRONMENT.yaml) == "true" ];
-then
-  echo "Syncing postgres..."
-  argocd app sync postgres \
-    --plaintext \
-    --port-forward \
-    --port-forward-namespace argocd
-fi
-
-if [ $(yq -r .gafaelfawr.enabled ../science-platform/values-$ENVIRONMENT.yaml) == "true" ];
-then
-  echo "Syncing gafaelfawr..."
-  argocd app sync gafaelfawr \
-    --plaintext \
-    --port-forward \
-    --port-forward-namespace argocd
-fi
+# Sync cert-issuer, but don't exit if this environment doesn't use it.
+argocd app sync cert-issuer \
+  --port-forward \
+  --port-forward-namespace argocd || true
 
 echo "Sync remaining science platform apps"
 argocd app sync -l "argocd.argoproj.io/instance=science-platform" \
-  --plaintext \
   --port-forward \
   --port-forward-namespace argocd
 
