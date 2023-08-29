64a32d50580f3b14ff8036fb43dd4c8149afc2e6
Russ Allbery <rra@lsst.org>
2023-08-29 12:35:49 -0700

    Fix typo in test_version name

--- tests/packaging_test.py
+++ tests/packaging_test.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from phalanx import __version__
 
 
-def test_vesrion() -> None:
+def test_version() -> None:
     """Test that the package has a version (and is installed)."""
     assert len(__version__) > 0
     assert __version__ != "0.0.0"  # would be if not installed
