759024fe605ff41af6e34424dd7a845811b06251
Russ Allbery <rra@lsst.org>
2023-09-27 12:20:22 -0700

    Produce nicer output from install.sh
    Be more consistent in the formatting and make the comments a bit
    more accurate. Stop doing an early sync of the pull secret, since
    vault-secrets-operator and Argo CD no longer need it.

--- installer/install.sh
+++ installer/install.sh
@@ -4,18 +4,21 @@ ENVIRONMENT=${1:?$USAGE}
 config="../environments/values-${ENVIRONMENT}.yaml"
 VAULT_ROLE_ID=${2:?$USAGE}
 VAULT_SECRET_ID=${3:?$USAGE}
-export VAULT_ADDR=$(yq -r .vaultUrl "$config")
-export VAULT_TOKEN=$(vault write auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" | grep 'token ' | awk '{ print $2 }')
-VAULT_PATH_PREFIX=$(yq -r .vaultPathPrefix "$config")
-ARGOCD_PASSWORD=$(vault kv get --field=admin.plaintext_password $VAULT_PATH_PREFIX/argocd)
 
+echo "Getting Git branch and remote information..."
 GIT_URL=$(git config --get remote.origin.url)
 # Github runs in a detached head state, but sets GITHUB_REF,
 # extract the branch from it.  If we're there, use that branch.
 # git branch --show-current will return empty in deatached head.
 GIT_BRANCH=${GITHUB_HEAD_REF:-$(git branch --show-current)}
 
-echo "Set VAULT_TOKEN in a secret for vault-secrets-operator..."
+echo "Logging on to Vault..."
+export VAULT_ADDR=$(yq -r .vaultUrl "$config")
+export VAULT_TOKEN=$(vault write auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" | grep 'token ' | awk '{ print $2 }')
+VAULT_PATH_PREFIX=$(yq -r .vaultPathPrefix "$config")
+ARGOCD_PASSWORD=$(vault kv get --field=admin.plaintext_password $VAULT_PATH_PREFIX/argocd)
+
+echo "Putting Vault credentials in a secret for vault-secrets-operator..."
 # The namespace may not exist already, but don't error if it does.
 kubectl create ns vault-secrets-operator || true
 kubectl create secret generic vault-credentials \
@@ -24,15 +27,9 @@ kubectl create secret generic vault-credentials \
   --from-literal=VAULT_SECRET_ID=$VAULT_SECRET_ID \
   --dry-run=client -o yaml | kubectl apply -f -
 
-echo "Set up docker pull secret for vault-secrets-operator..."
-vault kv get --field=.dockerconfigjson $VAULT_PATH_PREFIX/pull-secret > docker-creds
-kubectl create secret generic pull-secret -n vault-secrets-operator \
-    --from-file=.dockerconfigjson=docker-creds \
-    --type=kubernetes.io/dockerconfigjson \
-    --dry-run=client -o yaml | kubectl apply -f -
-
-echo "Update / install vault-secrets-operator..."
-# ArgoCD depends on pull-secret, which depends on vault-secrets-operator.
+# Argo CD depends a Vault-created secret for its credentials, so
+# vault-secrets-operator has to be installed first.
+echo "Updating or installing vault-secrets-operator..."
 helm dependency update ../applications/vault-secrets-operator
 helm upgrade vault-secrets-operator ../applications/vault-secrets-operator \
   --install \
@@ -44,7 +41,7 @@ helm upgrade vault-secrets-operator ../applications/vault-secrets-operator \
   --timeout 5m \
   --wait
 
-echo "Update / install argocd using helm..."
+echo "Updating or installing Argo CD using Helm..."
 helm dependency update ../applications/argocd
 helm upgrade argocd ../applications/argocd \
   --install \
@@ -56,7 +53,7 @@ helm upgrade argocd ../applications/argocd \
   --timeout 5m \
   --wait
 
-echo "Login to argocd..."
+echo "Logging in to Argo CD..."
 argocd login \
   --plaintext \
   --port-forward \
@@ -64,7 +61,7 @@ argocd login \
   --username admin \
   --password $ARGOCD_PASSWORD
 
-echo "Creating top level application"
+echo "Creating the top-level Argo CD application..."
 argocd app create science-platform \
   --repo $GIT_URL \
   --path environments --dest-namespace default \
@@ -78,11 +75,11 @@ argocd app create science-platform \
   --values values.yaml \
   --values values-$ENVIRONMENT.yaml
 
+echo "Syncing the top-level Argo CD application..."
 argocd app sync science-platform \
   --port-forward \
   --port-forward-namespace argocd
 
-echo "Syncing critical early applications"
 if [ $(yq -r '.applications."ingress-nginx"' "$config") != "false" ]; then
   echo "Syncing ingress-nginx..."
   argocd app sync ingress-nginx \
@@ -90,14 +87,15 @@ if [ $(yq -r '.applications."ingress-nginx"' "$config") != "false" ]; then
     --port-forward-namespace argocd
 fi
 
-# Wait for the cert-manager's webhook to finish deploying by running
-# kubectl, argocd's sync doesn't seem to wait for this to finish.
 if [ $(yq -r '.applications."cert-manager"' "$config") != "false" ]; then
   echo "Syncing cert-manager..."
   argocd app sync cert-manager \
     --port-forward \
     --port-forward-namespace argocd && \
-    kubectl -n cert-manager rollout status deploy/cert-manager-webhook
+
+  # Wait for the cert-manager's webhook to finish deploying by running
+  # kubectl, argocd's sync doesn't seem to wait for this to finish.
+  kubectl -n cert-manager rollout status deploy/cert-manager-webhook
 fi
 
 if [ $(yq -r .applications.postgres "$config") == "true" ]; then
@@ -114,11 +112,12 @@ if [ $(yq -r .applications.gafaelfawr "$config") != "false" ]; then
     --port-forward-namespace argocd
 fi
 
-echo "Sync remaining science platform apps"
+echo "Syncing remaining applications..."
 argocd app sync -l "argocd.argoproj.io/instance=science-platform" \
   --port-forward \
   --port-forward-namespace argocd
 
+echo ''
 echo "You can now check on your argo cd installation by running:"
 echo "kubectl port-forward service/argocd-server -n argocd 8080:443"
 echo "For the ArgoCD admin password:"
