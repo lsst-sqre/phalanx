f2cb1e739ba04c053ca1e8dc3964d60bc0b6acec
Russ Allbery <rra@lsst.org>
2023-12-08 14:31:41 -0800

    Add GCP information to documentation
    Add a way to fill out GCP hosting information for environments
    running on GKE, and expose that information in the generated
    environments documentation along with links to the Google Console
    and Log Explorer.

--- tests/docs/jinja_test.py
+++ tests/docs/jinja_test.py
@@ -44,10 +44,14 @@ def test_build_jinja_contexts(factory: Factory) -> None:
         assert idfdev.fqdn == "data-dev.lsst.cloud"
         assert idfdev.argocd_url == "https://data-dev.lsst.cloud/argo-cd"
         assert idfdev.identity_provider == IdentityProvider.CILOGON
+        assert idfdev.gcp.project_id == "science-platform-dev-7696"
+        assert idfdev.gcp.region == "us-central1"
+        assert idfdev.gcp.cluster_name == "science-platform"
         assert minikube.name == "minikube"
         assert minikube.fqdn == "minikube.lsst.cloud"
         assert minikube.argocd_url is None
         assert minikube.identity_provider == IdentityProvider.GITHUB
+        assert minikube.gcp is None
 
         # Check some of the more complex data.
         expected = read_output_data("idfdev", "argocd-rbac-rst")
