70c6a04107eb3d7fd0f080077f886e16824663dd
Russ Allbery <rra@lsst.org>
2024-08-07 09:38:32 -0700

    Test reordering application syncs

--- src/phalanx/services/environment.py
+++ src/phalanx/services/environment.py
@@ -175,9 +175,9 @@ class EnvironmentService:
         with action_group("Sync infrastructure applications"):
             for application in (
                 "ingress-nginx",
+                "gafaelfawr",
                 "cert-manager",
                 "postgres",
-                "gafaelfawr",
             ):
                 if application in environment.applications:
                     self._argocd.sync(application)
