f8743f5fa9e1d971ddf8363ffd0ea814bdba0a48
dspeck1 <dspeck@burwood.com>
2025-04-02 10:59:56 -0500

    Allows next-visit-fan-out-keda app to be disabled.  This to allow failback during migration and will be removed after migration complete.

--- tests/config_test.py
+++ tests/config_test.py
@@ -12,7 +12,7 @@ import yaml
 from phalanx.factory import Factory
 from phalanx.models.applications import Project
 
-_ALLOW_DISABLED = {"production-tools"}
+_ALLOW_DISABLED = {"production-tools", "next-visit-fan-out-keda"}
 """Temporary whitelist of applications not enabled anywhere."""
 
 _ALLOW_NO_SECRETS = {"next-visit-fan-out"}
